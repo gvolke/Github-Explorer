e91ecde29bedba2026c31525090962ec859777ca
Gustavo Volke <guvolke@gmail.com>
2023-10-25 10:26:27 -0300

    dealing with github api and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 
-import GlobalStyle from './styles/global'
-import Routes from './Routes';
+import GlobalStyle from "./styles/global";
+import Routes from "./Routes";
 
 const App: React.FC = () => (
-  <>
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>
-    <GlobalStyle />
-  </>
-)
-
+    <>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+        <GlobalStyle />
+    </>
+);
 
 export default App;
